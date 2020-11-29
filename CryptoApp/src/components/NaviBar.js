bd81467438134c2a6ee36b1e6bd504b7de9cf3d8
Ali Tunç <56436360+ali2nc@users.noreply.github.com>
2020-11-29 14:38:21 +0300

    name change

--- CryptoApp/src/components/NaviBar.js
+++ CryptoApp/src/components/NaviBar.js
@@ -4,14 +4,14 @@ import FontAwesome5 from 'react-native-vector-icons/FontAwesome5'
 const Height=Dimensions.get("window").height;
 const Width=Dimensions.get("window").width;
 
-function NaviBar() {
+function NaviBar({navigation}) {
     const [toggle,setToggle]=useState(false);
     return (
         <View style={{backgroundColor:"#232632"}}>
             <View style={{flexDirection:"row",padding:Height/40,paddingLeft:Height/50,paddingRight:Height/50,justifyContent:"space-between",alignItems:"center"}}>
                 <FontAwesome5 size={30} name={'coins'} color="#fbd208"/>
                 <Text style={{fontWeight:"bold",fontSize:30,color:"#fff"}}>
-                    CRYPTO
+                    CRYPTO App
                 </Text>
                 <TouchableWithoutFeedback onPress={() => setToggle(!toggle)}>
                     {
@@ -25,8 +25,8 @@ function NaviBar() {
             {
                 toggle ? (
                     <View style={{borderWidth:0.25,borderColor:"#232632",borderTopColor:"#d8d8d8"}}>
-                        <Text style={styles.texts}>Hakkımızda</Text>
-                        <Text style={styles.texts}>İletişim</Text>
+                        <Text style={styles.texts} onPress={() => navigation.navigate('KriptoList')}>Ana Sayfa</Text>
+                        <Text style={styles.texts} onPress={() => navigation.navigate('ExchangeP')}>Birim Çevirici</Text>
                         <View style={{flexDirection:"row",justifyContent:"center",alignItems:"center",marginBottom:25}}>
                             <FontAwesome5 onPress={() => Linking.openURL('http://www.twitter.com')} size={30} name={'twitter'} color="#d8d8d8" style={{padding:15}}/>
                             <FontAwesome5 onPress={() => Linking.openURL('http://www.facebook.com')} size={30} name={'facebook'} color="#d8d8d8" style={{padding:15}}/>
