ff17574ff06e187488f9ea524c9b81ec9daf2061
YKahveciSAU <75043387+YKahveciSAU@users.noreply.github.com>
2020-11-29 15:17:01 +0300

    Component
    Componentler eklendi

--- CryptoApp/App.js
+++ CryptoApp/App.js
@@ -1,17 +1,25 @@
-import React from 'react';
+import * as React from 'react';
 import { StyleSheet, Text, View, StatusBar,ScrollView  } from 'react-native';
 import Exchange from './pages/Exchange';
-import KriptoList from './pages/Kriptolar';
 import NaviBar from './src/components/NaviBar';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import Kriptolar from './pages/KriptoPage'
+import ExchangePage from './pages/Exchange'
+import 'react-native-gesture-handler';
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View >
+    <>
       <StatusBar style="auto"/>
-        <ScrollView persistentScrollbar={true}>
-            <NaviBar/>
-            <Exchange/>
-        </ScrollView>
-    </View>
+      <NavigationContainer >
+        <Stack.Navigator >
+          <Stack.Screen name="KriptoList" component={Kriptolar} options={{headerShown: false}}/>
+          <Stack.Screen name="ExchangeP" component={ExchangePage} options={{headerShown: false}}/>
+        </Stack.Navigator>
+      </NavigationContainer>
+    </>
   );
 }
